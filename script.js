ca7fd26f09f533183b1578ed45e30d7e9eb9a370
Prince of Kochi <mathenjs@gmail.com>
2020-10-14 22:13:01 -0700

    initial setup

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// API keys
+const civicKey = "AIzaSyBXX_LFscJIXrN_xa7JvFqda1GJXYE8L0Y";
+const apikey = "6woKQBsiMzXTaqOFIAGiI2GSdgPTj31EzZIVDGnF";
+
+// default to recieve all information for Google API
+const electionId = "2000";
+
+// Set address to empty string.
+var address = "";
